c86f3593bc189235445b1fa7ea2d54e3bef7d168
laksh_lukhi <140299225+lukhi-laksh@users.noreply.github.com>
2025-05-27 23:21:26 +0530

    Update Demo-1.py

--- Chapter-7/Demo-1.py
+++ Chapter-7/Demo-1.py
@@ -8,7 +8,7 @@ s = pd.Series(['p', 'q', 'r', 's', 'q'], dtype="category")
 # Display the Series
 print(s)
 
-# Apeend a Category
+# Apeend a Category, "t is add the last"
 s = s.cat.add_categories("t")
 
 # Display the Updated Category
