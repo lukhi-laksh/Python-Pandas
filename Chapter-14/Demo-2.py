336f4731f80bdb62a38305261733b28e55512865
lukhi-laksh <luxlukhi04@gmail.com>
2025-05-29 17:43:47 +0530

    Chapter-14 Complete

---
+++ Chapter-14/Demo-2.py
@@ -0,0 +1,19 @@
+# Pandas itertuples() to iterate over rows 
+
+import pandas as pd
+
+data = {
+    'Id': [101, 102, 103, 104, 105],
+    'Student': ["Amit", "John", "Laksh", "Taksh", "Darsh"],
+    'Roll': [5, 4, 3, 2, 1],
+    'Rank': [1, 2, 3, 4, 5], 
+    'Marks': [90, 85, 80, 75, 40],
+    'Address': ["East", "West", "North", "South", "NorthEast"]
+
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+for row in df.itertuples():
+    print(row)
---
+++ Chapter-14/Demo-3.py
@@ -0,0 +1,20 @@
+# Pandas items() to iterate over rows 
+
+import pandas as pd 
+
+data = {
+    'Id': [101, 102, 103, 104, 105],
+    'Student': ["Amit", "John", "Laksh", "Taksh", "Darsh"],
+    'Roll': [5, 4, 3, 2, 1],
+    'Rank': [1, 2, 3, 4, 5], 
+    'Marks': [90, 85, 80, 75, 40],
+    'Address': ["East", "West", "North", "South", "NorthEast"]
+
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+for a,b in df.items():
+    print(a)
+    print(b)
