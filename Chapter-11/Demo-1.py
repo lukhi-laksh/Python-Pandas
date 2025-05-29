4c54d4a85f69ec1e4a659fbe45298d547fa5544f
lukhi-laksh <luxlukhi04@gmail.com>
2025-05-29 15:20:31 +0530

    Chapter-11 Complete

---
+++ Chapter-11/Demo-1.py
@@ -0,0 +1,15 @@
+# Select More than one column
+
+import pandas as pd 
+
+
+data = {
+    'Student': ["Amit", "John", "Laksh", "Taksh", "Darsh"],
+    'Rank': [1, 2, 3, 4, 5], 
+    'Marks': [90, 85, 80, 75, 40],
+}
+
+df = pd.DataFrame(data)
+
+print(df)
+print(df[["Student", "Rank"]])
---
+++ Chapter-11/Demo-2.py
@@ -0,0 +1,19 @@
+# Select multiple columns in a range with Pandas
+
+import pandas as pd 
+
+
+data = {
+    'Id': [101, 102, 103, 104, 105],
+    'Student': ["Amit", "John", "Laksh", "Taksh", "Darsh"],
+    'Roll': [5, 4, 3, 2, 1],
+    'Rank': [1, 2, 3, 4, 5], 
+    'Marks': [90, 85, 80, 75, 40],
+    'Address': ["East", "West", "North", "South", "NorthEast"]
+
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+print(df[df.columns[2:5]])
