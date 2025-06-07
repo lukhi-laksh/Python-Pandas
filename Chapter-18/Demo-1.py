480722f6db8434ff29f0ff4a71d12682ecdd2ac5
laksh_lukhi <140299225+lukhi-laksh@users.noreply.github.com>
2025-06-07 23:37:44 +0530

    Chapter-18 Complete

--- Chapter-18/Demo-1.py
+++ Chapter-18/Demo-1.py
@@ -4,6 +4,8 @@ import pandas as pd
 
 data = ["Laksh", "Harsh", "Taksh", "Daksh", "Sahil"]
 
+# convert list into series
 series = pd.Series(data)
 
+# Print sereies in lower case
 print(series.str.lower())
